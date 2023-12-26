0e39b9dfc5890bfaad7a1281586d0cbd81136589
Debasishkaity22 <139854092+Debasishkaity22@users.noreply.github.com>
2023-12-26 16:42:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //your JS code here. If required.
 document.addEventListener("DOMContentLoaded",function(e){
 	let body1=document.body;
+	body1.innerHTML=``;
 	let p1=document.createElement("p");
 	p1.textContent="DOM load success";
 	body1.appendChild(p1);
